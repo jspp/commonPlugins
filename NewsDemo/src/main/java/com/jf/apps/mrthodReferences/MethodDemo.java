18623e967779f8232b8d45287e898f3be0e8a5e3
ouyang <jspp@qq.com>
2018-05-30 16:58:19 +0800

    注释 及 完善示例。

--- NewsDemo/src/main/java/com/jf/apps/mrthodReferences/MethodDemo.java
+++ NewsDemo/src/main/java/com/jf/apps/mrthodReferences/MethodDemo.java
@@ -8,12 +8,26 @@ import java.util.List;
  诸如String::length的语法形式叫做方法引用（method references），这种语法用来替代某些特定形式Lambda表达式。
  如果Lambda表达式的全部内容就是调用一个已有的方法，那么可以用方法引用来替代Lambda表达式。方法引用可以细分为四类：
 
- 方法引用类别	举例
- 引用静态方法	    Integer::sum
- 引用某个对象的方法	list::add
- 引用某个类的方法	String::length
- 引用构造方法   	HashMap::new
- 我们会在后面的例子中使用方法引用。
+ 引用静态方法
+ ContainingClass::staticMethodName
+ 例子: String::valueOf，对应的Lambda：(s) -> String.valueOf(s)
+ 比较容易理解，和静态方法调用相比，只是把.换为::
+
+ 引用特定对象的实例方法
+ containingObject::instanceMethodName
+ 例子: x::toString，对应的Lambda：() -> this.toString()
+ 与引用静态方法相比，都换为实例的而已
+
+ 引用特定类型的任意对象的实例方法
+ ContainingType::methodName
+ 例子: String::toString，对应的Lambda：(s) -> s.toString()
+ 太难以理解了。难以理解的东西，也难以维护。建议还是不要用该种方法引用。
+ 实例方法要通过对象来调用，方法引用对应Lambda，Lambda的第一个参数会成为调用实例方法的对象。
+
+ 引用构造函数
+ ClassName::new
+ 例子: String::new，对应的Lambda：() -> new String()
+ 构造函数本质上是静态方法，只是方法名字比较特殊。
  */
 public class MethodDemo {
 
@@ -23,7 +37,7 @@ public class MethodDemo {
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 3,7,4,5);
-        list.stream().forEach((X)->System.out.print(X));
+        list.stream().map(String::valueOf).forEach(System.out::println);
 
     }
 
