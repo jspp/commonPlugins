dd1edea8382fc67f88395280c3097f90e9367f48
jspp@qq.com <123456>
2019-01-15 18:51:27 +0800

    自动代理测试类

--- NewsDemo/src/test/java/com/jf/AppTest.java
+++ NewsDemo/src/test/java/com/jf/AppTest.java
@@ -5,6 +5,7 @@ import com.jf.apps.autoProxy.Test2Service;
 import com.jf.apps.autoProxy.TestService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,9 +21,18 @@ public class AppTest {
     @Resource
     private Test2Service test2Service;
 
+    @Resource
+    private BeanFactory beanFactory;
+
     @Test
     public void show() {
         testService.sayhello("123");
         test2Service.sayhello(1230);
+
+        System.out.println(beanFactory.getBean(TestService.class).hashCode());
+        System.out.println(beanFactory.getBean(Test2Service.class));
+
+        System.out.println(testService.hashCode());
+        System.out.println(test2Service);
     }
 }
